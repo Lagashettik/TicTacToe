60faaf88d01cee485fa1dba221b882378d40cb35
Lagashettik <Lagashettik@gmail.com>
2021-02-03 16:27:43 +0530

    Added: Player to choose Letter using Static block

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,7 +1,23 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class TicTacToe {
-    static  char[] Board = new char[10];
+    static char[] Board = new char[10];
+    static final char HumanPlay;
+    static final char ComputerPlay;
+
+    static Scanner scan = new Scanner(System.in);
+
+    static {
+        System.out.println("Enter your Choice X or O");
+        HumanPlay = scan.next().charAt(0);
+        if(HumanPlay == 'X')
+            ComputerPlay = 'O';
+        else
+            ComputerPlay = 'X';
+        System.out.println("Player : X\nComputer : O");
+    }
+
     TicTacToe(){
         Arrays.fill(Board,' ');
     }
