5a024f3c28cd95227b7f95a028981ef380c4d026
Lagashettik <Lagashettik@gmail.com>
2021-02-03 16:09:25 +0530

    Game Started with all cell empty

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,5 +1,12 @@
-public class TicTacToe {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class TicTacToe {
+    static  char[] Board = new char[10];
+    TicTacToe(){
+        Arrays.fill(Board,' ');
+    }
+
+    public static void main(String[] args) {
+    TicTacToe Game = new TicTacToe();
     }
 }
