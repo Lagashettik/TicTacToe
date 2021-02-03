bc8a0f6b407404726f17f4fef1e84f37281e4117
Lagashettik <Lagashettik@gmail.com>
2021-02-03 20:20:55 +0530

    Checking game result at every move

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -6,6 +6,7 @@ public class TicTacToe {
     static final char HumanPlay;
     static final char ComputerPlay;
     static int count;
+    static int tie;
 
     static Scanner scan = new Scanner(System.in);
 
@@ -25,9 +26,9 @@ public class TicTacToe {
 
     void Toss(){
         int Toss =(int) Math.floor(Math.random() * 10) % 2;
-        System.out.println("Tossing.....\n");
+        System.out.println("\nTossing.....\n");
         if(Toss == 0)
-            System.out.println("Computer win the Toss\nComputer makes First move");
+            System.out.println("Computer won the Toss\nComputer makes First move");
         else
         {
             System.out.println("You won the Toss\nYou make First move");
@@ -44,45 +45,64 @@ public class TicTacToe {
             else
                 MakeMove(HumanPlay);
             ShowBoard();
-            count++;
-
-            if(i == 9)
+            if(i == 9){
                 for(int j = 1; j < 10; j++)
                     if(Board[j] == ' ')
                         i--;
 
+                if(i == 9)
+                        tie++;
+                }
+
+            if(Result() == ' ')
+                break;
+
+            count++;
         }
     }
 
-    void MakeMove(char id){
-        boolean itr;
-        int cell;
-        if(id == HumanPlay) {
-            System.out.println("\nEnter Cell Number");
-            do {
-                cell = scan.nextInt();
-                itr = true;
-                if (Board[cell] == ' '){
-                    Board[cell] = HumanPlay;
-                }
-                else {
-                    itr = false;
-                    System.out.println("\nCell is not Empty\nEnter other cell number");
-                }
-            }while (!itr);
-        }
-        else{
-            do {
-                cell =(int) Math.floor(Math.random() * 10 ) % 9;
-                itr = true;
-                if (Board[cell] == ' ')
-                    Board[cell] = ComputerPlay;
-                else {
-                    itr = false;
-                }
+    void MakeMove(char Player){
+        if(Player == HumanPlay)
+            HumanPlays();
+        else
+            ComputerPlays();
+    }
 
-            }while (!itr);
-        }
+    void ComputerPlays(){
+        System.out.println("Computer Playing");
+        boolean itr = false;
+        int cell;
+        do {
+            cell = (int) (Math.floor(Math.random() * 10) % 9) + 1;
+            if (Board[cell] == ' ') {
+                Board[cell] = ComputerPlay;
+                itr = true;
+            }
+
+        }while (!itr);
+        System.out.println("Computer Played");
+    }
+
+    void HumanPlays(){
+        boolean itr;
+        System.out.println("\nEnter Cell Number");
+        do {
+            int Cell = scan.nextInt();
+             itr = false;
+
+            if(Cell > 0 && Cell < 10){
+                if (Board[Cell] == ' '){
+                    Board[Cell] = HumanPlay;
+                    itr = true;
+                }
+                else
+                    System.out.println("\nCell is not Empty\nEnter other Cell number");
+
+            }
+            else
+                System.out.println("Enter Correct Index Number");
+
+        }while (!itr);
     }
 
     void ShowBoard(){
@@ -96,6 +116,45 @@ public class TicTacToe {
             System.out.print(" "+Board[i]+"|");
         }
         System.out.println("\n");
+    }
+
+    char Result(){
+        if(ResultCheck() == HumanPlay) {
+            System.out.println("Player Won");
+            return ' ';
+        }
+        else if (ResultCheck() == ComputerPlay){
+                System.out.println("Computer Won");
+                return ' ';
+        }
+        else if(ResultCheck() == 'T') {
+            System.out.println("Match is Tie");
+            return ' ';
+        }
+        return 0;
+    }
+
+    char ResultCheck(){
+        if (Board[1] == Board[2] && Board[2] == Board[3])
+            return Board[1];
+        else if(Board[1] == Board[4] && Board[4] == Board[7])
+            return Board[1];
+        else if (Board[1] == Board[5] && Board[5] == Board[9])
+            return Board[1];
+        else if(Board[2] == Board[5] && Board[5] == Board[8])
+            return Board[2];
+        else if(Board[3] == Board[6] && Board[6] == Board[9])
+            return Board[3];
+        else if (Board[3] == Board[5] && Board[5] == Board[7])
+            return Board[3];
+        else if (Board[4] == Board[5] && Board[5] ==  Board[6])
+            return Board[4];
+        else if (Board[7] == Board[8] && Board[8] == Board[9])
+            return Board[7];
+        else if(tie != 0)
+            return 'T';
+        else
+            return ' ';
     }
 
     public static void main(String[] args) {
